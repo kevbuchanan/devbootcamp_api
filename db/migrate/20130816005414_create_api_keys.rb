60e9744c57ac7ad6e708e68809ce3095cd3a83da
William Karsten <yme_ynot45@hotmail.com>
2013-08-16 11:46:46 -0500

    remove duplicated files This migration now exists on the socrates site and is redundant on the api site.

--- db/migrate/20130816005414_create_api_keys.rb
+++
@@ -1,9 +0,0 @@
-class CreateApiKeys < ActiveRecord::Migration
-  def change
-    create_table :api_keys do |t|
-      t.string   :key
-      t.integer  :user_id, null: false
-      t.timestamps
-    end
-  end
-end
