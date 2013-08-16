16b75d8252154b3e38f498f9e002221c566e2a98
William Karsten <yme_ynot45@hotmail.com>
2013-08-15 21:29:23 -0500

    Added http authentication

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,5 +1,5 @@
 class ApiKey < ActiveRecord::Base
-  before_create :generate_access_token
+  after_initialize :generate_access_token
   belongs_to :user
   validates_uniqueness_of :key
   validates_presence_of   :key, :user_id
