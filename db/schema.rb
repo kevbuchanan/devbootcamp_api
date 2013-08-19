0ed146b203091b7a7360538ea5962e0016177edc
William Karsten <yme_ynot45@hotmail.com>
2013-08-19 14:09:34 -0500

    TDD api_keys custom authorization helpers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130816005414) do
+ActiveRecord::Schema.define(:version => 20130816160201) do
 
   create_table "actors", :force => true do |t|
     t.string   "name",        :null => false
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(:version => 20130816005414) do
 
   create_table "api_keys", :force => true do |t|
     t.string   "key"
-    t.string   "user_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.integer  "user_id",    :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "challenge_attempts", :force => true do |t|
--- spec/helpers/authorization_helper_spec.rb
+++ spec/helpers/authorization_helper_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe AuthorizationHelper do
-  let(:user)    { create :user }
-  let(:api_key) { user.key }
+  let(:user)    { User.create(name: "abi", password: 'pizza', password_confirmation: 'pizza') }
+  let!(:api_key) { ApiKey.create(key: SecureRandom.hex, user: user) }
 
   describe "#valid_api_key?" do
     it "return true when request authorization header contains valid api key" do
@@ -14,8 +14,15 @@ describe AuthorizationHelper do
       controller.request.stub(:headers).and_return({"Authorization" => "DBC-API foobar"})
       expect(helper.valid_api_key?).to be_false
     end
+
+    it "return false when there is no authorization header in request" do
+      controller.request.stub(:headers).and_return("Some-Other-Header" => "foobar")
+      expect(helper.valid_api_key?).to be_false
+    end
+
+    it "should return false if authorization header is malformed" do
+      controller.request.stub(:headers).and_return({"Authorization" => "foobar"})
+      expect(helper.valid_api_key?).to be_false
+    end
   end
-
-  describe "#require_shared_key"
-
 end
