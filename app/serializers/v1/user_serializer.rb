46a5a0e22b0e351de882f78dd760ac5443eab47d
kevinbuch <kevaustinbuch@gmail.com>
2013-08-19 17:38:20 -0500

    added profile to user serializer

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,3 +1,11 @@
 class V1::UserSerializer < V1::BaseSerializer
-  attributes :name, :email, :bio, :profile, :cohort_id
+  attributes :name, :email, :bio, :cohort_id, :profile
+
+  def profile
+    profile_attributes = {}
+    User::Profile::ATTRIBUTES.each do |attr|
+      profile_attributes[attr] = object.send(attr)
+    end
+    profile_attributes
+  end
 end
