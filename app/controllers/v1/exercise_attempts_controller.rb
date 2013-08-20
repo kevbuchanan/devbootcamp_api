694b19488584ae7ef0aa9dad3393b059c7a16e49
Abraham Sangha <abraham.sangha@gmail.com>
2013-08-20 17:42:34 -0500

    Refactored to respond_with rather than render

--- app/controllers/v1/exercise_attempts_controller.rb
+++ app/controllers/v1/exercise_attempts_controller.rb
@@ -1,33 +1,12 @@
 class V1::ExerciseAttemptsController < V1::BaseController
+
   def index
     user = User.find(params[:user_id])
-
-    # if user
-      # exercise_attempts = user.exercise_attempts.correct.page({page: params[:page], per_page: params[:per_page]})
-      # render json: exercise_attempts
-      respond_with user.exercise_attempts.correct.page({page: params[:page], per_page: params[:per_page]})
-    # else
-    #   render(
-    #     :status => 404,
-    #     json:{
-    #       :message => "User not found",
-    #       :more_info => "http://errorpage.com"})
-    # end
+    respond_with user.exercise_attempts.correct.page({page: params[:page], per_page: params[:per_page]})
   end
 
   def show
     user = User.find(params[:user_id])
-
-    # if user
-      # exercise_attempts = user.exercise_attempts.correct.find(params[:id])
-      # render json: exercise_attempts
-      respond_with user.exercise_attempts.correct.find(params[:id])
-    # else
-    #   render(
-    #     :status => 404,
-    #     json:{
-    #       :message => "User not found",
-    #       :more_info => "http://errorpage.com"})
-    # end
+    respond_with user.exercise_attempts.correct.find(params[:id])
   end
 end
