f4cca1fa83344b26148f61966de4cde2b3d2072c
Abraham Sangha <abraham.sangha@gmail.com>
2013-08-20 16:55:49 -0500

    Removed commented-out code

--- app/controllers/v1/api_keys_controller.rb
+++ app/controllers/v1/api_keys_controller.rb
@@ -1,12 +1,5 @@
 class V1::ApiKeysController < V1::BaseController
   def show
     respond_with ApiKey.find_by_user_id!(params[:id])
-
-    # if api_key
-    #   render json: api_key
-    # else
-    #   render nothing: true, status: 404
-    # end
   end
-
 end
