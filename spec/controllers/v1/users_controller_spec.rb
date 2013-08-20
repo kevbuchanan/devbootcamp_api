10489611ef731f82ef0c24e1fe4250b9c165c890
kevinbuch <kevaustinbuch@gmail.com>
2013-08-19 22:28:24 -0500

    added spec for user profile

--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -33,6 +33,11 @@ describe V1::UsersController, :helper_namespace => :api_v1 do
       expect(name).to_not be_nil
     end
 
+    it 'should return a users profile' do
+      body = JSON.parse(response.body)
+      expect(body['user']).to have_key('profile')
+    end
+
     it 'should return a 404 status if the user is not found' do
       get :show, :id => 0, :format => :json
       expect(response.status).to eql(404)
