210cb927caa7820139db9eccf0bc1f113558621e
kevinbuch <kevaustinbuch@gmail.com>
2013-08-20 13:15:03 -0500

    added cohort id

--- app/serializers/v1/cohort_serializer.rb
+++ app/serializers/v1/cohort_serializer.rb
@@ -1,4 +1,4 @@
 class V1::CohortSerializer < V1::BaseSerializer
-  attributes :name, :location, :start_date, :email, :in_session
+  attributes :id, :name, :location, :start_date, :email, :in_session
   has_many :users, key: :students
 end
