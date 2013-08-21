77c697a0bc028d77f44075f03e0d91dd212cdf35
kevinbuch <kevaustinbuch@gmail.com>
2013-08-20 23:32:02 -0500

    added before filters to all controllers

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -16,6 +16,15 @@ class V1::BaseController < ApplicationController
       		 } unless valid_api_key?
   end
 
+  def require_shared_key
+    render status: 401,
+       json: {
+         status: 401,
+         message: "Invalid Shared key",
+         more_info: "https://dev.devbootcamp.com/documentation#errors"
+       } unless valid_shared_key?
+  end
+
   def render_404
 	  render status: 404,
 	     		 json: {
