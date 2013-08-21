3bdf0f861806fef3ffb81fa72581ae3dcc429cc2
kevinbuch <kevaustinbuch@gmail.com>
2013-08-21 15:11:55 -0500

    added newline after before filters

--- app/controllers/v1/exercises_controller.rb
+++ app/controllers/v1/exercises_controller.rb
@@ -1,5 +1,6 @@
 class V1::ExercisesController < V1::BaseController
   before_filter :require_api_key
+
   def index
     respond_with Exercise.published
   end
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class V1::UsersController < V1::BaseController
   before_filter :require_api_key
+
   def index
     respond_with User.all
   end
